f9619bd3f07dcb3c783df6c29a18b227dee87c12
unknown <tjsgnl2002@gmail.com>
2019-12-05 00:40:54 +0900

    filter button add

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -1,9 +1,55 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(3),
+    minWidth: 120
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
+  }
+}));
 
 const FilterBar = () => {
-  return <div>여기는 필터</div>;
+  const classes = useStyles();
+  const [age, setAge] = React.useState("");
+
+  const handleChange = event => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Age
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-placeholder-label-label"
+          id="demo-simple-select-placeholder-label"
+          value={age}
+          onChange={handleChange}
+          displayEmpty
+          className={classes.selectEmpty}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
 
 // 3. 정렬 기능 :  평점순 오름차순, 내림차순 정렬  / (좋아요 숫자 오름차순 내림차순 정렬)
+// Action, Adventure, Drama, Music, Thriller 5개 장르 필터
 
 export default FilterBar;
